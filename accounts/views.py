e364a814ba202dccfceedbea912f90b2f0c49af3
심민성 <75308777+owl1753@users.noreply.github.com>
2022-05-22 21:15:33 +0900

    20220522

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,5 @@
 from asyncio.windows_events import NULL
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
 from rest_framework import serializers
 from .models import Account
 from django.views.decorators.csrf import csrf_exempt
@@ -29,7 +28,29 @@ def login(request):
             return HttpResponse(status=400)
 
     return HttpResponse(status=400)
-            
 
+@csrf_exempt
+def register(request):
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        search_email = data['email']
+        obj = NULL
+        try:
+            obj = Account.objects.get(email=search_email)
+        except Account.DoesNotExist:
+            if data['password'] != data['re_password']:
+                return HttpResponse(status=400)
 
+            Account.objects.create(
+                email = data['email'],
+                name = data['name'],
+                password = data['password']
+            )
 
+            obj = Account.objects.get(email=search_email)
+            serializer = AccountSerializer(obj)
+            return JsonResponse(serializer.data, safe=False)
+
+        return HttpResponse(status=400)
+
+    return HttpResponse(status=400)
